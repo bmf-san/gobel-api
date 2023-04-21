fd8bb2f6a5ba248b4a360d26cecc59a7e26da601
bmf-san <bmf.infomation@gmail.com>
2023-04-21 22:22:27 +0900

    [update] add fulltext index

--- mysql/initdb.d/1_schema.sql
+++ mysql/initdb.d/1_schema.sql
@@ -134,3 +134,4 @@ ALTER TABLE posts ADD INDEX index_posts_category_id(category_id);
 ALTER TABLE comments ADD INDEX index_comments_status(status);
 ALTER TABLE tag_post ADD INDEX index_tag_post_tag_id(tag_id);
 ALTER TABLE tag_post ADD INDEX index_tag_post_post_id(post_id);
+ALTER TABLE posts ADD FULLTEXT INDEX index_title_md_body (title, md_body) WITH PARSER ngram;
