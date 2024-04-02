219ecda5c0d0170ff05938a60e5ffe839abd1ae1
bmf-san <bmf.infomation@gmail.com>
2024-04-02 14:36:30 +0900

    [update] 1_schema.sql

--- mysql/initdb.d/1_schema.sql
+++ mysql/initdb.d/1_schema.sql
@@ -31,21 +31,6 @@ CREATE TABLE `posts` (
   FOREIGN KEY (category_id) REFERENCES categories(id)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8;
 
-DROP TABLE IF EXISTS `archived_posts`;
-CREATE TABLE `archived_posts` (
-  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `admin_id` int(11) UNSIGNED NOT NULL,
-  `category_id` int(11) UNSIGNED NOT NULL,
-  `title` varchar(255) DEFAULT NULL,
-  `md_body` longtext DEFAULT NULL,
-  `html_body` longtext DEFAULT NULL,
-  `status` varchar(255) DEFAULT "draft",
-  `created_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  FOREIGN KEY (admin_id) REFERENCES admins(id),
-  FOREIGN KEY (category_id) REFERENCES categories(id)
-) ENGINE = InnoDB DEFAULT CHARSET = utf8;
-
 DROP TABLE IF EXISTS `comments`;
 CREATE TABLE `comments` (
   `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -55,17 +40,6 @@ CREATE TABLE `comments` (
   `created_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   FOREIGN KEY (post_id) REFERENCES posts(id)
-) ENGINE = InnoDB DEFAULT CHARSET = utf8;
-
-DROP TABLE IF EXISTS `archived_comments`;
-CREATE TABLE `archived_comments` (
-  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `archived_post_id` int(11) UNSIGNED NOT NULL,
-  `body` longtext NOT NULL,
-  `status` varchar(255) DEFAULT "pending",
-  `created_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  FOREIGN KEY (archived_post_id) REFERENCES archived_posts(id)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8;
 
 DROP TABLE IF EXISTS `tags`;
@@ -85,17 +59,6 @@ CREATE TABLE `tag_post` (
   `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   FOREIGN KEY (tag_id) REFERENCES tags(id),
   FOREIGN KEY (post_id) REFERENCES posts(id)
-) ENGINE = InnoDB DEFAULT CHARSET = utf8;
-
-DROP TABLE IF EXISTS `archived_tag_post`;
-CREATE TABLE `archived_tag_post` (
-  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `tag_id` int(11) UNSIGNED NOT NULL,
-  `archived_post_id` int(11) UNSIGNED NOT NULL,
-  `created_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  FOREIGN KEY (tag_id) REFERENCES tags(id),
-  FOREIGN KEY (archived_post_id) REFERENCES archived_posts(id)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8;
 
 DROP VIEW IF EXISTS `view_posts`;
